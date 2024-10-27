b7820a3136801054e81a1bf427cef9811992678f
ohammou- <ohammou-@e1r6p8.1337.ma>
2024-10-27 14:31:36 +0100

    add ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,13 +1,46 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 
 int main(int ac, char **av)
 {
-    if (ac != 4)
-        return (0);
-    std::string filename = av[1];
-    std::string s1 = av[2];
-    std::string s2 = av[3];
+	if (ac != 4)
+		return (0);
+	std::string str;
+	std::string filename = av[1];
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+	std::string res;
+	std::ifstream file;
+	int pos = 0;
+	int b = 0;
 
-    return (0);
+	file.open(filename);
+	if (!file.is_open()) {std::cerr << "file not open"<< "\n"; return 1;}
+	std::ofstream OutFile(filename + ".replace");
+	if (!OutFile)
+	{
+		std::cerr << "Error creating file: " << filename << ".replace" << "\n";
+		return (1);
+	}
+
+	while (std::getline(file, str))
+	{
+		while(str.find(s1) != std::string::npos)
+		{
+			pos = str.find(s1);
+			if (pos != std::string::npos)
+			{
+				res += str.substr(0, pos) + s2;
+				str = str.substr(pos + 1);
+			}
+		}
+		if (!res.empty())
+		{
+			OutFile << res;
+			
+		}
+	}
+	std::cout << res;
+	return (0);
 }
