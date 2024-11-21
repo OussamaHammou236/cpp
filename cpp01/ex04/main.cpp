6962e80f19833e393ff6c3774bb3910863348b47
ohammou- <ohammou-@e1r8p11.1337.ma>
2024-11-21 19:53:25 +0100

    handel same errors in cpp

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,7 +1,8 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-
+#include <iostream>
+#include <fstream>
 int main(int ac, char **av)
 {
 	if (ac != 4)
@@ -12,9 +13,9 @@ int main(int ac, char **av)
 	std::string s2 = av[3];
 	std::string res;
 	std::ifstream file;
-	int pos = 0;
-	int b = 0;
+	std::string::size_type pos = 0;
 
+	if (s1.empty() || s2.empty()) {std::cout << "empty string no valid" ; return -1;} 
 	file.open(filename);
 	if (!file.is_open()) {std::cerr << "file not open"<< "\n"; return 1;}
 	std::ofstream OutFile(filename + ".replace");
@@ -30,15 +31,15 @@ int main(int ac, char **av)
 		while (pos != std::string::npos)
 		{
 			res += str.substr(0, pos) + s2;
-			str = str.substr(pos + 1);
+			str = str.substr(pos + s1.length());
 			pos = str.find(s1);
 		}
 		if (pos == std::string::npos)
-		{
 			res += str;
-		}
 		res += "\n";
 	}
+	if (!str.empty())
+		res.pop_back();
 	OutFile << res;
 	return (0);
 }
