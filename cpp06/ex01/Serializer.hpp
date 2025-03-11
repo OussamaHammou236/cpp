30bbd6c09fd03239df356db93b96ae2fde3570b3
ohammou- <ohammou-@e1r8p7.1337.ma>
2025-03-11 00:28:31 +0000

    add the ex01

---
+++ cpp06/ex01/Serializer.hpp
@@ -0,0 +1,11 @@
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+class Serializer
+{
+    public:
+        static uintptr_t serialize(Data* ptr);
+        static Data* deserialize(uintptr_t raw);
+}
+
+#endif
