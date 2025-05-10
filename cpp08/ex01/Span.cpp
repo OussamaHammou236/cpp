95f545dbe7550a0138341e7c6a955dfb64125888
ohammou- <ohammou-@e1r8p9.1337.ma>
2025-05-10 21:50:23 +0100

    ff

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -1,21 +1,44 @@
 #include "Span.hpp"
 
-// template <typename T>
-// Span<T> :: Span() : container(), N(0)
-// {
+Span:: Span() : vec(), N(0) {}
+Span:: Span(unsigned int N) : vec(), N(N) {}
 
-// }
 
-// template <typename T>
-// Span<T>:: Span(unsigned int N) : container(), N(N)
-// {
-// }
+void Span::addNumber(int number)
+{
+    if (vec.size() >= N)
+        throw std::out_of_range("the contaner is full");
+    vec.push_back(number);
+}
 
-// template <typename T>
-// void Span<T>::addNumber(int number)
-// {
-//     static int cont = 0;
-//     if (cont >= N)
-//         throw std::out_of_range("the container is full");
-//     container.push_back(number);
-// }
+void Span::print_numbers()
+{
+    for (int i = 0; i < vec.size(); i++)
+        std::cout << vec[i] << std::endl;
+}
+
+int Span::longestSpan()
+{
+    if (vec.size() < 2)
+        return 0;
+    std::vector<int> tmp = vec;
+    sort(tmp.begin(), tmp.end());
+    return tmp.at(tmp.size() - 1) - tmp.at(0);
+}
+
+int Span::shortestSpan()
+{
+    if (vec.size() < 2)
+        return 0;
+    std::vector<int> tmp = vec;
+    sort(tmp.begin(), tmp.end());
+    int cont;
+    int shortest = tmp.at(1) - tmp.at(0); 
+    for (int i = 1;i < tmp.size() - 1; i++)
+    {
+        cont = tmp.at(i + 1) - tmp.at(i);
+        if (cont < shortest)
+            shortest = cont;
+    }
+    return shortest;
+}
